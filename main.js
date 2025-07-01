735f9f5ad16e7c03ca91b8bcae4bf59200b1063e
matmacote <mat.macote@lgusanfernandobuk.gov.ph>
2025-07-01 14:25:30 +0800

    "Initial commit"

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const coll = document.querySelectorAll(".collapsible");
+
+    coll.forEach(btn => {
+        btn.addEventListener("click", function () {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            content.style.display = content.style.display === "block" ? "none" : "block";
+        });
+    });
+});
